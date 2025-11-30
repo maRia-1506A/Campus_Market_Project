fc9143962e74e1449a3bce69b024c31d01be7bfd
maRia-1506A <zannatulfarzana363@gmail.com>
2025-11-30 19:37:08 +0600

    progress of client part

--- Campus_Market_Client/tailwind.config.js
+++ Campus_Market_Client/tailwind.config.js
@@ -1,14 +1,9 @@
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
-export default{
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [require('daisyui')],
-    daisyui: {
-        themes: ["light", "dark", "cupcake"],
-    },
-}
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
+  plugins: [daisyui],
+  daisyui: { themes: ["light", "dark", "cupcake"] },
+};
--- Campus_Market_Server/index.js
+++ Campus_Market_Server/index.js
@@ -1,8 +1,48 @@
-const express= require('express');
-const cors= require('cors');
-const jwt=require('jsonwebtoken');
-const {MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
-const app= express();
-const port= process.env.PORT || 5001;
+//server
+const app = express();
+const port = process.env.PORT || 5001;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://maria104:m0ng0db@cluster0.8ks1nbl.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // await client.connect();
+    const database = client.db("campusMarketDB");
+    const productCollection = database.collection("products");
+    const userCollection = database.collection("users");
+    const chatCollection = database.collection("chats");
+
+    console.log("Connected to MongoDB!");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Campus Market Server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
